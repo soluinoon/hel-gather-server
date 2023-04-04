666d392d3b7a87036bdd810056f27c53dd550531
core <jinjs6@gmail.com>
2023-04-04 16:05:32 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/mate/helgather/HelGatherApplication.java
@@ -0,0 +1,13 @@
+package com.mate.helgather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelGatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelGatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mate/helgather/HelGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mate.helgather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelGatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
