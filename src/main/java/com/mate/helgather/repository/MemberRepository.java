492b1de523dd383f9ef956e8621fe5171dbc86e9
eyeofsol <kingjihong@naver.com>
2023-04-05 18:07:28 +0900

    fix: UserRepository.java -> MemberRepository 로 변경
    레포지토리 역시 수정하였습니다.

--- src/main/java/com/mate/helgather/repository/MemberRepository.java
+++ src/main/java/com/mate/helgather/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package com.mate.helgather.repository;
 import com.mate.helgather.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
