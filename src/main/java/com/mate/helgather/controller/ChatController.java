5b7332a6e11ca751e4502cca8236755dd2115a05
eyeofsol <kingjihong@naver.com>
2023-04-27 22:26:15 +0900

    feat(Chat): 채팅 pub/sub 기능 구현
    실제 채팅을 보내고 받는 기능을 구현하였습니다.
    
    먼저, chat/send로 쓰여진 매핑에서 ChatDTO를 받아와서
    메세지를 /sub/chat/room으로 전달해 채팅방의 인원들에게 전파합니다.
    채팅 부분은 프론트에서 Sock.js와 같은 프론트가 담당하는 비중이 크다보니 그 부분을 더 공부해야 할 것 같습니다.

--- src/main/java/com/mate/helgather/controller/ChatController.java
+++ src/main/java/com/mate/helgather/controller/ChatController.java
@@ -1,11 +1,15 @@
 package com.mate.helgather.controller;
 
 import com.mate.helgather.domain.Member;
+import com.mate.helgather.dto.ChatDTO;
 import com.mate.helgather.dto.MemberDTO;
 import com.mate.helgather.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/chats")
 @Slf4j
 /*
 v1 특징
@@ -24,5 +27,11 @@ v1 특징
 public class ChatController {
 
     private final ChatService chatService;
+    private final SimpMessageSendingOperations template;
 
+    @MessageMapping("/chat/send") // 실제론 메세지 매핑으로 pub/chat/send 임
+    public void sendMessage(@Payload ChatDTO chatDTO) {
+        log.info("chat {} send by {} to room number{}", chatDTO.getMessage(), chatDTO.getSender(), chatDTO.getRoomId());
+        template.convertAndSend("/sub/chat/room" + chatDTO.getRoomId(), chatDTO);
+    }
 }
--- src/main/java/com/mate/helgather/dto/ChatDTO.java
+++ src/main/java/com/mate/helgather/dto/ChatDTO.java
@@ -1,18 +1,15 @@
 package com.mate.helgather.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class ChatDTO {
-    public enum MessageType{
-        ENTER, TALK, LEAVE;
-    }
-
-    private MessageType type; // 메시지 타입
-    private String roomId; // 방 번호
+    private long roomId; // 방 번호
     private String sender; // 채팅을 보낸 사람
     private String message; // 메시지
-    private String time;
+    private String time; // 시간은 프론트에서 담아서 보내줄
 }
