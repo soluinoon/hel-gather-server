5474b0425c05f25c36834d04cbaad4ed7f517327
eyeofsol <kingjihong@naver.com>
2023-05-22 15:21:43 +0900

    fix: 오류 수정
    git merge 중 일어난 오류를 수정합니다.

--- src/main/java/com/mate/helgather/service/ChatService.java
+++ src/main/java/com/mate/helgather/service/ChatService.java
@@ -5,14 +5,15 @@ import com.mate.helgather.domain.status.ChatRoomStatus;
 import com.mate.helgather.domain.status.MemberStatus;
 import com.mate.helgather.domain.status.RecruitmentStatus;
 import com.mate.helgather.dto.ChatDto;
-import com.mate.helgather.dto.ChatRoomDto;
-import com.mate.helgather.dto.MessagesResponse;
 import com.mate.helgather.exception.BaseException;
 import com.mate.helgather.exception.ErrorCode;
-import com.mate.helgather.repository.*;
+import com.mate.helgather.repository.ChatRoomRepository;
+import com.mate.helgather.repository.MemberRepository;
+import com.mate.helgather.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.mate.helgather.dto.MessagesResponse;
+import com.mate.helgather.repository.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -25,47 +26,10 @@ public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final MessageRepository messageRepository;
     private final MemberRepository memberRepository;
-    private final ApplicationRepository applicationRepository;
     private final MemberChatRoomRepository memberChatRoomRepository;
     private final RecruitmentRepository recruitmentRepository;
-    /**
-     채팅방을 크루지원 id값을 이용해 가져온다.
-     만약, 채팅방이 없다면 예외를 발생
-     멤버가 자기 페이지에서 채팅을 들어갈 때 사용
-     */
-//    @Transactional
-//    public ChatRoomDto getChatRoomsByMemberId(Long memberId) {
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(NoSuchElementException::new);
-//        return new ChatRoomDto(member.getChatRoom().getId());
-//    }
 
-    /**
-    채팅방을 지원서를 이용해 만든다.
-    지원서를 참고해 지원한 유저와 모집글의 유저로 채팅방을 만든 뒤,
-    각각의 유저의 채팅방을 설정해준다.
-
-    이 메서드는 아마 지원서의 지원과 수락을 받는다면 일어날 것이다.
-     */
-//    @Transactional
-//    public ChatRoom createChatRoom(Long applicationId) {
-//        Application application = applicationRepository.findById(applicationId).orElseThrow(IllegalAccessError::new);
-//        //
-//        Member recruitMember = application.getRecruitment().getMember();
-//        Member applyMember = application.getMember();
-//
-//        ChatRoom savedChatRoom = chatRoomRepository.save(ChatRoom.builder()
-//                .recruitment(application.getRecruitment())
-//                .build());
-//        recruitMember.setChatRoom(savedChatRoom);
-//        applyMember.setChatRoom(savedChatRoom);
-//        return savedChatRoom;
-//    }
-
-    public void saveMessage(ChatDto chatDTO) throws BaseException {
-        if (!chatRoomRepository.existsById(chatDTO.getRoomId())) {
-            throw new BaseException(ErrorCode.NO_SUCH_CHATROOM_ERROR);
-        }
+    public void saveMessage(ChatDto chatDTO) {
         messageRepository.save(Message.builder().chatRoom(chatRoomRepository.findById(chatDTO.getRoomId()).orElseThrow(NoSuchElementException::new))
                 .member(memberRepository.getReferenceById(chatDTO.getUserId()))
                 .description(chatDTO.getMessage())
