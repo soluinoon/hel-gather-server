b7520a423dbc6d9dad1bd52f4f17faeaba33ffcc
core <jinjs6@gmail.com>
2023-05-24 14:26:46 +0900

    fix: 회원가입 예외 처리 관련 result 출력 수정

--- src/main/java/com/mate/helgather/exception/ExceptionController.java
+++ src/main/java/com/mate/helgather/exception/ExceptionController.java
@@ -1,16 +1,10 @@
 package com.mate.helgather.exception;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.net.http.HttpResponse;
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class ExceptionController {
