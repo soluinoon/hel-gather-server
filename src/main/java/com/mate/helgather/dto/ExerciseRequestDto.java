2f658e16de9aebc0eb7165b030920304910fb15b
eyeofsol <kingjihong@naver.com>
2023-05-25 14:52:24 +0900

    feat(Exercise): 카테고리 추가 형태로 변형 및 SBD 조회 기능 구현
    프론트와의 협업을 위해 Exercise 테이블에 Category 필드를 추가하였습니다.
    BENCH_PRESS, SQUAT, DEAD_LIFE, TODAY로 나눠지며
    TODAY의 경우엔 비디오 URL을 비우도록 설정했습니다.

---
+++ src/main/java/com/mate/helgather/dto/ExerciseRequestDto.java
@@ -0,0 +1,11 @@
+package com.mate.helgather.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExerciseRequestDto {
+    String videoUrl;
+    String thumbNailUrl;
+}
--- src/main/java/com/mate/helgather/repository/ExerciseRepository.java
+++ src/main/java/com/mate/helgather/repository/ExerciseRepository.java
@@ -1,10 +1,15 @@
 package com.mate.helgather.repository;
 
 import com.mate.helgather.domain.Exercise;
+import com.mate.helgather.domain.status.ExerciseCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
-    List<Exercise> findAllByMemberId(Long memberId);
+    List<Exercise> findAllByMemberIdAndCategory(Long memberId, ExerciseCategory category);
+    Optional<Exercise> findTopByMemberIdAndCategoryOrderByCreatedAtDesc(Long memberId, ExerciseCategory category);
+    void deleteByVideoUrl(String videoUrl);
+    void deleteByMemberIdAndVideoUrlAndThumbnailUrl(Long memberId, String videoUrl, String thumbnailUrl);
 }
