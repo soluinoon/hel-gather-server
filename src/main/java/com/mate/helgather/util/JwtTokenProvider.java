f94e629f026ad63acd6aab780905edf5089de31e
core <jinjs6@gmail.com>
2023-05-26 17:24:44 +0900

    fix: jwt accessToken, refreshToken 유효기간 일주일로 변경

--- src/main/java/com/mate/helgather/util/JwtTokenProvider.java
+++ src/main/java/com/mate/helgather/util/JwtTokenProvider.java
@@ -38,7 +38,7 @@ public class JwtTokenProvider {
         long now = (new Date()).getTime();
 
         //Access Token 생성
-        Date accessTokenExpiresIn = new Date(now + 86400000);
+        Date accessTokenExpiresIn = new Date(now + 604800000);
         String accessToken = Jwts.builder()
                 .setSubject(authentication.getName())
                 .claim("auth", authorities)
@@ -48,7 +48,7 @@ public class JwtTokenProvider {
 
         //Refresh Token 생성
         String refreshToken = Jwts.builder()
-                .setExpiration(new Date(now + 86400000))
+                .setExpiration(new Date(now + 604800000))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
