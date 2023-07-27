c57e8be48c35b235c57ae15de58b03391fc45b45
eyeofsol <kingjihong@naver.com>
2023-07-27 13:31:27 +0900

    test(Recruitment): 모집 컨트롤러 테스트 작성
    모집 컨트롤러의 테스트 코드를 작성했습니다.
    Spring REST Docs와 결합하여 작성하였습니다.

--- src/main/java/com/mate/helgather/controller/ExerciseController.java
+++ src/main/java/com/mate/helgather/controller/ExerciseController.java
@@ -5,6 +5,7 @@ import com.mate.helgather.dto.TodayExerciseResponseDto;
 import com.mate.helgather.exception.BaseResponse;
 import com.mate.helgather.service.ExerciseService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,27 +15,29 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
+@RequestMapping("/exercises")
 public class ExerciseController {
     private final ExerciseService exerciseService;
 
-    @GetMapping("/exercises")
+    @GetMapping
     public ResponseEntity<BaseResponse> findAllByMemberId(@RequestParam("member") Long memberId) {
-        //TODO: memberId 1에서 오류 발생
         List<TodayExerciseResponseDto> todayExerciseResponseDtos = exerciseService.findAllByMemberId(memberId);
         return new ResponseEntity<>(new BaseResponse(todayExerciseResponseDtos), HttpStatus.OK);
     }
 
-    @PostMapping("/exercises")
-    public ResponseEntity<BaseResponse> save(@RequestParam("member") Long memberId,
-                                                          @RequestPart("file") MultipartFile multipartFile) {
+    @PostMapping
+    public ResponseEntity<BaseResponse> create(@RequestParam("member") Long memberId,
+                                               @RequestPart("file") MultipartFile multipartFile) {
         TodayExerciseResponseDto todayExerciseResponseDto = exerciseService.save(memberId, multipartFile);
         return new ResponseEntity<>(new BaseResponse(todayExerciseResponseDto), HttpStatus.OK);
     }
 
-    @DeleteMapping("/exercises")
-    public ResponseEntity<BaseResponse> delete(@PathVariable("member") Long memberId,
-                                                         TodayExerciseRequestDto todayExerciseRequestDto) throws Exception {
-        exerciseService.delete(memberId, todayExerciseRequestDto);
+    @DeleteMapping
+    public ResponseEntity<BaseResponse> delete(@RequestParam("member") Long memberId,
+                                               @RequestParam("imageUrl") String imageUrl) {
+        //TODO: 임시 개편
+        exerciseService.delete(memberId, new TodayExerciseRequestDto(imageUrl));
         return new ResponseEntity<>(new BaseResponse("삭제 성공"), HttpStatus.OK);
     }
 }
--- src/main/java/com/mate/helgather/dto/TodayExerciseRequestDto.java
+++ src/main/java/com/mate/helgather/dto/TodayExerciseRequestDto.java
@@ -1,10 +1,14 @@
 package com.mate.helgather.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class TodayExerciseRequestDto {
     private String imageUrl;
 }
