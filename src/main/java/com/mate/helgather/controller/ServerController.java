80bf377a6d16e03bdac6e383f62df227860b3ba5
eyeofsol <kingjihong@naver.com>
2023-07-30 12:42:33 +0900

    fix: profiles 오류 수정
    profile -> profiles로 변경했고, 정상적으로 작동하는 것을 확인했습니다.
    profile 적용 후 /getServerInfo를 통해 어떤 서버(프로필)가 동작하고 있는지 확인할 수 있습니다.

--- src/main/java/com/mate/helgather/controller/ServerController.java
+++ src/main/java/com/mate/helgather/controller/ServerController.java
@@ -1,7 +1,6 @@
 package com.mate.helgather.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +9,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@PropertySource(value = "application.yml")
 public class ServerController {
-
     @Value("${serverName}")
     private String serverName;
+    @Value("${dbInfo}")
+    private String dbInfo;
     private Integer visitedCount = 0;
 
     @GetMapping("/getServerInfo")
-    public ResponseEntity<Map<String, String>> getServerInfo(){
+    public ResponseEntity<Map<String, String>> getServerInfo() {
         visitedCount++;
 
         Map<String, String> serverInfo = new HashMap<>();
         serverInfo.put("ServerName:", serverName);
         serverInfo.put("visitedCount:", visitedCount.toString());
+        serverInfo.put("dbInfo", dbInfo);
 
         return ResponseEntity.ok(serverInfo);
     }
